85538a44dd9d6079991ab1b369c3a55c5fce84e3
Kliment <kligjo@gmail.com>
2025-01-22 19:36:07 +0100

    .

---
+++ ConsoleApp1/Personcs.cs
@@ -0,0 +1,45 @@
+﻿
+namespace ConsoleApp1.Models
+{
+    public class Person
+    {
+        private string Name { get; set; }
+        private int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public void SetAge(int age)
+        {
+            Age = age;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine("Hello!");
+        }
+
+        public void Greet(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public string RespondingBack()
+        {
+            return $"Hello, my name is {Name}!";
+        }
+    }
+}

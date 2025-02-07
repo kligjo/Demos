1cc63ddbc5325742c7a494970f3a8dd0ccaadc9c
kligjo <kligjo@gmail.com>
2025-02-07 18:16:20 +0100

    Create vezba.cs

---
+++ vezba.cs
@@ -0,0 +1,51 @@
+// Exercise 1: Basic Student Information
+// Topics: Variables, basic data types, console input/output
+// Create a program that asks for and displays student information
+static void Excercire1()
+{
+// TODO: Ask for student's name (string)
+// TODO: Ask for student's age (int)
+// TODO: Ask for student's grade average (double)
+// TODO: Display all information in a formatted way
+}
+
+// Exercise 2: Student Grade Calculator
+// Topics: Arrays, loops, methods
+// Build upon Exercise 1 by adding multiple grades and calculating the average
+static void Excercire2()
+{
+// TODO: Ask for student's name
+// TODO: Create an array to store 5 grades
+// TODO: Use a loop to input the grades
+// TODO: Create a method to calculate average
+// TODO: Display student's name and calculated average
+}
+
+// TODO: Create Student class with properties:
+    // - Name (string)
+    // - Age (int)
+    // - Grades (List<double>)
+    // TODO: Add constructor
+    // TODO: Add method to add grades
+    // TODO: Add method to calculate average
+static void Excercire3()
+{
+    // TODO: Create student instance
+    // TODO: Add grades
+    // TODO: Display student information
+}
+
+// TODO: Create Classroom class
+    // TODO: Add methods to:
+    //     - Add students
+    //     - Remove students
+    //     - Find student by name
+    //     - Calculate class average
+    // TODO: Add basic error handling
+static void Excercire4()
+{
+// TODO: Create classroom
+        // TODO: Add multiple students
+        // TODO: Demonstrate finding students
+        // TODO: Show class average
+}
